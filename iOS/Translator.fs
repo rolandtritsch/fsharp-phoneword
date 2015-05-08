18f2a779c4d368b64afa8f256155160ea6652f17
Roland Tritsch <roland@tritsch.org>
2015-05-08 12:52:51 +0100

    Checkpoint: Porting it to the latest and greatest version of Xamarin

---
+++ iOS/Translator.fs
@@ -0,0 +1,17 @@
+﻿namespace Phoneword
+
+open System
+ 
+module Translator = 
+    let ToNumber number =
+        String.map (fun c -> match c with
+                             | 'A' | 'B' | 'C' -> '2'
+                             | 'D' | 'E' | 'F' -> '3'
+                             | 'G' | 'H' | 'I' -> '4'
+                             | 'J' | 'K' | 'L' -> '5'
+                             | 'M' | 'N' | 'O' -> '6'
+                             | 'P' | 'Q' | 'R' | 'S' -> '7'
+                             | 'T' | 'U' | 'V' -> '8'
+                             | 'W' | 'X' | 'Y' | 'Z' -> '9'
+                             | _ -> c
+                   ) number
--- iOS2/iOS2ViewController.fs
+++ iOS2/iOS2ViewController.fs
@@ -1,22 +1,18 @@
-﻿namespace Phoneword
+﻿namespace iOS2
 
 open System
 open System.Drawing
 open UIKit
 open Foundation
 
-[<Register("iOSViewController")>]
-type iOSViewController() = 
+[<Register("iOS2ViewController")>]
+type iOS2ViewController() = 
     inherit UIViewController()
     // Release any cached data, images, etc that aren't in use.
     override this.DidReceiveMemoryWarning() = base.DidReceiveMemoryWarning()
-
     // Perform any additional setup after loading the view, typically from a nib.
-    override this.ViewDidLoad() = 
-        base.ViewDidLoad()
-        //TranslateButton.TouchUpInside (Object sender, EventArgs e) -> 
-            //PhoneNumberText.SetText (Translator.ToNumber PhoneNumberText.GetText)
-
+    override this.ViewDidLoad() = base.ViewDidLoad()
     // Return true for supported orientations
     override this.ShouldAutorotateToInterfaceOrientation(orientation) = 
         orientation <> UIInterfaceOrientation.PortraitUpsideDown
+
